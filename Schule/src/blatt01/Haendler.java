ca73bbc49cdc287d3f8a734b6507e71ec6ca3f09
5u5m0n <28sidill@gmail.com>
2025-09-01 09:33:28 +0200

    blatt01 weitergemacht

--- Schule/src/blatt01/Haendler.java
+++ Schule/src/blatt01/Haendler.java
@@ -2,17 +2,17 @@ package blatt01;
 
 public class Haendler {
     public static void main(String[] args) {
-        double sd_preis = 5;
-        double minisd_preis = 8;
-        double microsd_preis = 12;
-        int anz_sd = 9;
-        int anz_minisd = 5;
-        int anz_microsd = 0;
-        double gesamtpreis = (sd_preis * anz_sd) + (minisd_preis * anz_minisd) + (microsd_preis * anz_microsd);
+        double sdPreis = 5;
+        double minisdPreis = 8;
+        double microsdPreis = 12;
+        int anzSd = 9;
+        int anzMinisd = 5;
+        int anzMicrosd = 0;
+        double gesamtpreis = (sdPreis * anzSd) + (minisdPreis * anzMinisd) + (microsdPreis * anzMicrosd);
         System.out.println("Der Preis für...");
-        System.out.println(anz_sd + " normale SD-Karten");
-        System.out.println(anz_minisd + " MiniSD-Karten");
-        System.out.println(anz_microsd + " MicroSD-Karten");
+        System.out.println(anzSd + " normale SD-Karten");
+        System.out.println(anzMinisd + " MiniSD-Karten");
+        System.out.println(anzMicrosd + " MicroSD-Karten");
         System.out.println("...lautet " + gesamtpreis + " €");
         //1.: 2
         //2.: int; double
--- Schule/src/blatt01/Zinssatz.java
+++ Schule/src/blatt01/Zinssatz.java
@@ -6,9 +6,16 @@ public class Zinssatz {
         double zinsatz = 2.25;
         double zinsen = (kapital * zinsatz)/100;
         double neu = kapital + zinsen;
+        /*
         System.out.println("Altes Kapital: " + kapital + " €");
         System.out.println("Zinssatz: " + zinsatz + " %");
         System.out.println("\nZinsen: " + zinsen + " €");
         System.out.println("Neues Kapital: " + neu + " €");
+        */
+        System.out.printf("Altes Kaptial %.2f €\n", kapital);
+        System.out.printf("Zinssatz: %.2f %%\n\n",zinsatz);
+        System.out.printf("Zinsen: %.2f €\n", zinsen);
+        System.out.printf("Neues Kapital: %.2f €",neu);
+
     }
 }
