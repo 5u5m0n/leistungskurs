92468de79a7c4d344ab7f20a247b28b71ee8a511
5u5m0n <28sidill@gmail.com>
2025-09-09 12:35:54 +0200

    blatt04

--- Schule/src/blatt04/ETF.java
+++ Schule/src/blatt04/ETF.java
@@ -1,20 +1,42 @@
 package blatt04;
 
+ import java.util.Random;
+import java.util.Scanner;
+
 public class ETF {
     public static void main(String[] args) {
+        Random rnd = new Random();
+        Scanner input = new Scanner(System.in);
         double sparbetrag = 50;
         double wert = 3578.89;
-        double gesamtsparbetrag = 0;
+        double kontostand = 0;
         double anteil;
+        double zinssatz;
         int monat = 1;
-        while (monat <= 12) {
-            gesamtsparbetrag += sparbetrag;
-            anteil = gesamtsparbetrag / wert;
-            System.out.println("Monat: " + monat);
-            System.out.printf("Sparbetrag diesen Monat: %.2f €\n", sparbetrag);
-            System.out.printf("Gesamtsparbetrag: %.2f €\n", gesamtsparbetrag);
-            System.out.printf("Anteil: %.5f %%\n\n", anteil);
-            monat ++;
+        int jahr = 1;
+        System.out.println("Wie viele Jahre lang möchtest du sparen?");
+        int anzahlJahre = input.nextInt();
+
+        while (jahr <= anzahlJahre) {
+            System.out.println("--------------------------------------------------");
+            System.out.println("Wie viel möchtest du dieses Jahr pro Monat sparen?");
+            sparbetrag = input.nextDouble();
+            while (monat <= 12) {
+                zinssatz = ((rnd.nextDouble() + 1) * 6) / 100;
+                kontostand = (kontostand + sparbetrag) * (zinssatz + 1);
+                anteil = kontostand / wert;
+                System.out.println("--------------------------------------------------");
+                System.out.println("Jahr: " + jahr);
+                System.out.println("Monat: " + monat);
+                System.out.printf("Sparbetrag diesen Monat: %.2f €\n", sparbetrag);
+                System.out.printf("Zinssatz: %.2f %%\n", zinssatz * 100);
+                System.out.printf("Kontostand: %.2f €\n", kontostand);
+                System.out.printf("MSCI-Anteil: %.5f %%\n", anteil);
+                monat++;
+            }
+            monat = 1;
+            jahr++;
         }
+
     }
 }
