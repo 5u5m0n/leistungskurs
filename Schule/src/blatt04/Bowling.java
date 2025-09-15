9e5e4092c7fa237de50766a6b4bc4c646abbedf2
5u5m0n <28sidill@gmail.com>
2025-09-15 18:12:52 +0200

    Bowling

--- Schule/src/blatt04/Bowling.java
+++ Schule/src/blatt04/Bowling.java
@@ -11,24 +11,42 @@ public class Bowling {
         int pins = 10;
         int multiplikator = 1;
         int strike = 0;
-        //TODO: Multiplikator
         while (frames < 10) {
+            frames++;
+            System.out.println("Frame " + frames);
             while (wuerfe < 2) {
+                wuerfe++;
+                switch (strike) {
+                    case 0:
+                        multiplikator = 1;
+                        break;
+                    case 1, 2:
+                        strike--;
+                        multiplikator = 2;
+                        break;
+                    case 3:
+                        multiplikator = 3;
+                }
+                System.out.println("----------------------------------");
+                System.out.println("Wurf " + wuerfe);
                 System.out.println("Wie viele Pins hast du umgeworfen?");
                 int treffer = input.nextInt();
                 pins -= treffer;
                 punkte += treffer*multiplikator;
-                if (wuerfe == 0 && pins == 0) {
-
+                System.out.println("Punkte: " + punkte);
+                System.out.println("----------------------------------");
+                if (wuerfe == 1 && pins == 0) {
+                    strike += 2;
+                    System.out.println("STRIKE!");
                     break;
-                } else if (wuerfe == 1 && pins == 0) {
-
+                } else if (wuerfe == 2 && pins == 0) {
+                    strike += 1;
+                    System.out.println("SPARE!");
                 }
 
             }
             pins = 10;
             wuerfe = 0;
-            frames++;
         }
     }
 }
