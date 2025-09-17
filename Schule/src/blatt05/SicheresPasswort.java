5948998b0fa4f5410d2eea227ecf9f7161dc9b2a
5u5m0n <28sidill@gmail.com>
2025-09-17 11:18:12 +0200

    Passwort

---
+++ Schule/src/blatt05/SicheresPasswort.java
@@ -0,0 +1,12 @@
+package blatt05;
+
+import java.util.Scanner;
+
+public class SicheresPasswort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Passwort eingeben.");
+        String a = input.nextLine();
+        if (a.length() > 8) {}
+    }
+}
