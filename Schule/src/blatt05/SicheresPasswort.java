6c46cb4cffa516295bfeea7ff957b5eb214de4e2
5u5m0n <28sidill@gmail.com>
2025-09-18 16:32:07 +0200

    passwort

--- Schule/src/blatt05/SicheresPasswort.java
+++ Schule/src/blatt05/SicheresPasswort.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class SicheresPasswort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        String a;
         boolean length = false;
         boolean uppercase = false;
         boolean lowercase = false;
         boolean digit = false;
         boolean special = false;
-        while (!length && !uppercase && !lowercase && !digit && !special) {
+        while (true) {
+            length = false;
+            uppercase = false;
+            lowercase = false;
+            digit = false;
+            special = false;
             System.out.println("Passwort eingeben.");
-            String a = input.nextLine();
+            a = input.nextLine();
             for (int i = 0; i < a.length(); i++) {
                 char c = a.charAt(i);
                 if (c >= 'A' && c <= 'Z') {
@@ -29,14 +34,11 @@ public class SicheresPasswort {
             if (a.length() >= 8) {
                 length = true;
             }
-            if (!length || !uppercase || !lowercase || !digit || !special) {
+            if (length && uppercase && lowercase && digit && special) {
+                break;
+            } else {
                 System.out.println("Passwort nict sicher genug.");
             }
-            length = false;
-            uppercase = false;
-            lowercase = false;
-            digit = false;
-            special = false;
         }
         System.out.println("Passwort gesichert.");
     }
