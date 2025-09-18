22897589303a47d3ece5f6beae867a7c58e9b4d2
5u5m0n <28sidill@gmail.com>
2025-09-18 16:16:57 +0200

    passwort

--- Schule/src/blatt05/SicheresPasswort.java
+++ Schule/src/blatt05/SicheresPasswort.java
@@ -7,6 +7,27 @@ public class SicheresPasswort {
         Scanner input = new Scanner(System.in);
         System.out.println("Passwort eingeben.");
         String a = input.nextLine();
-        if (a.length() > 8) {}
+        boolean length = false;
+        boolean uppercase = false;
+        boolean lowercase = false;
+        boolean digit = false;
+        boolean special = false;
+        while (!length && !uppercase && !lowercase && !digit && !special) {
+            for (int i = 0; i < a.length(); i++) {
+                char c = a.charAt(i);
+                if (c >= 'A' && c <= 'Z') {
+                    uppercase = true;
+                } else if (c >= 'a' && c <= 'z') {
+                    lowercase = true;
+                } else if (c >= '0' && c <= '9') {
+                    digit = true;
+                } else {
+                    special = true;
+                }
+            }
+            if (a.length() >= 0) {
+                length = true;
+            }
+        }
     }
 }
