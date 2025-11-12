c567c632fc458c2f4fed4ce56ff34bd8a9fd2e93
5u5m0n <28sidill@gmail.com>
2025-11-12 11:07:07 +0100

    blatt10

---
+++ Schule/src/blatt12/InsertionSort.java
@@ -0,0 +1,26 @@
+package blatt12;
+
+import static blatt07.ArbeitMitArrays.printArray;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        int eingabe;
+        int j;
+        for (int i = 1; i < arr.length; i++) {
+            eingabe = arr[i];
+            j = i;
+            while (j > 0 && arr[j - 1] > eingabe) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = eingabe;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arrSort = new int[] {7, 8, 2, 4, 7, 10};
+        printArray(arrSort);
+        insertionSort(arrSort);
+        printArray(arrSort);
+    }
+}
