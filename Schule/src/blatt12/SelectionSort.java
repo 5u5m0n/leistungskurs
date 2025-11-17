5728454348477aa52f6021c4a1be9bb70d9eec17
5u5m0n <28sidill@gmail.com>
2025-11-17 09:35:23 +0100

    blatt12

--- Schule/src/blatt12/SelectionSort.java
+++ Schule/src/blatt12/SelectionSort.java
@@ -1,19 +1,25 @@
 package blatt12;
 
+import static blatt07.ArbeitMitArrays.printArray;
+
 public class SelectionSort {
 
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             int min = i;
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = i; j < arr.length; j++) {
                 if (arr[j] < arr[min]) {
                     min = j;
                 }
             }
-            int h = arr[min];
-            arr[min] = arr[i];
-            arr[i] = h;
+            blatt11.BubbleSort.swap(arr, min, i);
+            printArray(arr);
         }
+    }
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 1, 4, 5, 2, 56, 52, 11, 55};
+        selectionSort(arr);
+        printArray(arr);
     }
 }
