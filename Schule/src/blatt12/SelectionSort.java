87eebc66816ff4a82ca56c03e2d654406458d278
5u5m0n <28sidill@gmail.com>
2025-11-18 11:46:52 +0100

    blatt12

--- Schule/src/blatt12/SelectionSort.java
+++ Schule/src/blatt12/SelectionSort.java
@@ -4,22 +4,28 @@ import static blatt07.ArbeitMitArrays.printArray;
 
 public class SelectionSort {
 
-    public static void selectionSort(int[] arr) {
+    /**
+     * Sortiert einen gegebenen int-Array von groß nach klein (max-Sort) oder klein nach groß (min-Sort)
+     * @param arr int-Array
+     * @param max true: max-Sort, false; min-Sort
+     */
+    public static void selectionSort(int[] arr, boolean max) {
         for (int i = 0; i < arr.length; i++) {
             int min = i;
             for (int j = i; j < arr.length; j++) {
-                if (arr[j] < arr[min]) {
+                if (arr[j] < arr[min] == !max) {
                     min = j;
                 }
             }
             blatt11.BubbleSort.swap(arr, min, i);
-            printArray(arr);
         }
     }
 
     public static void main(String[] args) {
         int[] arr = new int[]{3, 1, 4, 5, 2, 56, 52, 11, 55};
-        selectionSort(arr);
+        selectionSort(arr, false);
+        printArray(arr);
+        selectionSort(arr, true);
         printArray(arr);
     }
 }
