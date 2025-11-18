85cbfdfee9b03528a08c7a37230955c1ba71140b
5u5m0n <28sidill@gmail.com>
2025-11-18 13:00:40 +0100

    blatt12

--- Schule/src/blatt13/Zufall.java
+++ Schule/src/blatt13/Zufall.java
@@ -4,22 +4,51 @@ import static blatt07.ArbeitMitArrays.printArray;
 
 public class Zufall {
 
+    /**
+     * Gibt eine Zufallszahl zwischen 0 und b zurück
+     * @param b b
+     * @return Zahl
+     */
     public static double zufall(int b) {
         return Math.random() * b;
     }
 
+    /**
+     * Gibt eine zufällige Ganzzahl zwischen 0 und b zurück
+     * @param b b
+     * @return Zahl
+     */
     public static int zufallGanz(int b) {
         return (int) Math.round(Math.random() * b);
     }
 
+    /**
+     * Gibt eine Zahl zwischen a und b zurück
+     * @param a a
+     * @param b b
+     * @return Zahl
+     */
     public static double zufall(int a, int b) {
         return Math.random() * Math.abs(b-a) + a;
     }
 
+    /**
+     * Gibt eine zufällige Ganzzahl zwischen a und b zurück
+     * @param a a
+     * @param b b
+     * @return Zahl
+     */
     public static int zufallGanz(int a, int b) {
         return (int) Math.round(Math.random() * Math.abs(b-a) + a);
     }
 
+    /**
+     * Gibt einen Ganzzahl-Array der länge l, bestehend aus zufälligen Ganzzahlen zwischen a und b, zurück
+     * @param l l
+     * @param a a
+     * @param b b
+     * @return Zahl
+     */
     public static int[] zufallArray(int l, int a, int b) {
         int[] arr = new int[l];
         for (int i = 0; i < l; i++) {
@@ -29,6 +58,6 @@ public class Zufall {
     }
 
     public static void main(String[] args) {
-        printArray(zufallArray((int) Math.pow(10, 9), 1, 3));
+        printArray(zufallArray((int) Math.pow(10, 8), 1, 3));
     }
 }
