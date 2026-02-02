1854492dc2c299f7ec33e02df7212ad2ed62e8e8
5u5m0n <28sidill@gmail.com>
2026-02-02 11:24:14 +0100

    MIRROR FORCE

--- Schule/src/blatt15/Spiegel.java
+++ Schule/src/blatt15/Spiegel.java
@@ -8,8 +8,7 @@ import static blatt14.Simulationen.*;
 
 public class Spiegel {
 
-    public static void zufallSpiegel(char[][] spiegel, double p, int a) {
-        spiegel = createEmpty2DCharArray(a, a);
+    public static void zufallSpiegel(char[][] spiegel, double p) {
         for (int i = 1; i < spiegel.length - 1; i++) {
             for (int j = 1; j < spiegel[i].length - 1; j++) {
                 if (zufall(1) <= p && getNorden(spiegel, i, j, false) != '/' && getNorden(spiegel, i, j, false) != '\\' && getSueden(spiegel, i, j, false) != '/' && getSueden(spiegel, i, j, false) != '\\' && getWesten(spiegel, i, j, false) != '/' && getWesten(spiegel, i, j, false) != '\\' && getOsten(spiegel, i, j, false) != '/' && getOsten(spiegel, i, j, false) != '\\') {
@@ -26,7 +25,7 @@ public class Spiegel {
     public static void zufallZiele(char[][] spiegel, double p) {
         for (int i = 1; i < spiegel.length - 1; i++) {
             for (int j = 1; j < spiegel[i].length - 1; j++) {
-                if (zufall(1) <= p && getNorden(spiegel, i, j, false) != 'O' && getSueden(spiegel, i, j, false) != 'O' && getWesten(spiegel, i, j, false) != 'O' && getOsten(spiegel, i, j, false) != 'O') {
+                if (zufall(1) <= p && getNorden(spiegel, i, j, false) != 'O' && getSueden(spiegel, i, j, false) != 'O' && getWesten(spiegel, i, j, false) != 'O' && getOsten(spiegel, i, j, false) != 'O' && spiegel[i][j] == ' ') {
                     spiegel[i][j] = 'O';
                 }
             }
@@ -37,15 +36,99 @@ public class Spiegel {
         spiegel[0][spiegel.length / 2] = '>';
     }
 
+    public static char getDirection(char[][] spiegel, int dir, int i, int j) {
+        switch (dir) {
+            case 1:
+                return getSueden(spiegel, i, j, false);
+            case 2:
+                System.out.println("OSTEN");
+                return getOsten(spiegel, i, j, false);
+            case -1:
+                return getWesten(spiegel, i, j, false);
+            case -2:
+                return getNorden(spiegel, i, j, false);
+        }
+        return '0';
+    }
+
+    //TODO: FIX DAS PROGRAMM!
+    public static void bewegePfeile(char[][] spiegel) {
+        /*
+                1
+            2   /   -1   == Wenn dir = 1 || dir = -2 : - (-1); Else : + (-1)
+               -2
+ /
+                1
+            2   \   -1   == * (-1)
+               -2
+         */
+        int dir = 0;
+        for (int i = 0; i < spiegel.length; i++) {
+            for (int j = 0; j < spiegel[i].length; j++) {
+                dir = 0;
+                if (spiegel[i][j] == '>') {
+                    dir = 2;
+                } else if (spiegel[i][j] == '<') {
+                    dir = -1;
+                } else if (spiegel[i][j] == '^') {
+                    dir = -2;
+                }  else if (spiegel[i][j] == 'V') {
+                    dir = 1;
+                }
+                if (dir != 0) {
+                    char feld = getDirection(spiegel, dir, i, j);
+                    System.out.println(feld);
+                    int[] mx = new int[]{0, -1, 0, 0, 1};
+                    int[] my = new int[]{1, 0, 0, -1, 0};
+                    char[] zeichen = new char[]{'^', '<', '0', 'V', '>'};
+                    if (i + mx[dir + 2] < spiegel.length && j + my[dir + 2] < spiegel[i].length && i + mx[dir + 2] > 0 && j + my[dir + 2] > 0) {
+                        if (feld == ' ') {
+                            spiegel[i + mx[dir + 2]][j + my[dir + 2]] = spiegel[i][j];
+                            spiegel[i][j] = ' ';
+                        } else if (feld == 'O') {
+                            spiegel[i + mx[dir + 2]][j + my[dir + 2]] = ' ';
+                            spiegel[i][j] = ' ';
+
+                        } else if (feld == '/') {
+                            if (dir == 1 || dir == -2) {
+                                dir++;
+                            } else {
+                                dir--;
+                            }
+                            spiegel[i][j] = ' ';
+                            int x = i + mx[dir + 2];
+                            int y = j + my[dir + 2];
+                            spiegel[x + mx[dir + 2]][y + my[dir + 2]] = zeichen[dir + 2];
+                        } else if (feld == '\\') {
+                            dir *= -1;
+                            spiegel[i][j] = ' ';
+                            int x = i + mx[dir + 2];
+                            int y = j + my[dir + 2];
+                            spiegel[x + mx[dir + 2]][y + my[dir + 2]] = zeichen[dir + 2];
+                        } else {
+                            spiegel[i + mx[dir + 2]][j + my[dir + 2]] = ' ';
+                            spiegel[i][j] = ' ';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     public static void spiegelSimulation(int n, double q, int abs) {
         char[][] spiegel = createEmpty2DCharArray(40, 40);
         SchischVisualizer sv = new SchischVisualizer();
-        zufallSpiegel(spiegel, 0.3, 40);
+        zufallSpiegel(spiegel, 0.3);
         sv.step(spiegel);
-        //zufallZiele(spiegel, 0.1);
-        //sv.step(spiegel);
-        generierePfeil(spiegel);
+        zufallZiele(spiegel, 0.1);
         sv.step(spiegel);
+        for (int i = 0; i < n; i++) {
+            bewegePfeile(spiegel);
+            if (i % abs == 0) {
+                generierePfeil(spiegel);
+            }
+            sv.step(spiegel);
+        }
         sv.start();
     }
 
