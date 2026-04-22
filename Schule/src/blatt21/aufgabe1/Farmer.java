2b2dbe6a4b6b959fe75392d04f2336a962ee9e76
5u5m0n <28sidill@gmail.com>
2026-04-22 13:38:07 +0200

    TuermeVonHanoi

--- Schule/src/blatt21/aufgabe1/Farmer.java
+++ Schule/src/blatt21/aufgabe1/Farmer.java
@@ -8,6 +8,61 @@ public class Farmer {
     private Giesskanne giesskanne;
     private int energie;
 
+    public Farmer(String vorname, String farmname) {
+        this.vorname = vorname;
+        this.farmname = farmname;
+        this.vermoegen = 1000;
+        this.energie = 50;
+        this.axt = new Axt();
+        this.giesskanne = new Giesskanne();
+    }
 
+    public String getVorname() {
+        return vorname;
+    }
+
+    public String getFarmname() {
+        return farmname;
+    }
+
+    public double getVermoegen() {
+        return vermoegen;
+    }
+
+    public Axt getAxt() {
+        return axt;
+    }
+
+    public Giesskanne getGiesskanne() {
+        return giesskanne;
+    }
+
+    public int getEnergie() {
+        return energie;
+    }
+
+    public boolean istAxtAusgeruestet() {
+        return this.axt.istAusgeruestet();
+    }
+
+    public boolean istGiesskanneAusgeruestet() {
+        return this.giesskanne.istAusgeruestet();
+    }
+
+    public void giesskanneAusruesten() {
+        this.giesskanne.setAusgeruestet(true);
+    }
+
+    public void giesskanneAblegen() {
+        this.giesskanne.setAusgeruestet(false);
+    }
+
+    public void axtAusruesten() {
+        this.axt.setAusgeruestet(true);
+    }
+
+    public void axtAblegen() {
+        this.axt.setAusgeruestet(false);
+    }
 
 }
