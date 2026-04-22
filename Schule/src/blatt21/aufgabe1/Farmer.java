dac258dcb26f23acd199b36930dd0b276735a99a
5u5m0n <28sidill@gmail.com>
2026-04-22 11:48:15 +0200

    TuermeVonHanoi

---
+++ Schule/src/blatt21/aufgabe1/Farmer.java
@@ -0,0 +1,13 @@
+package blatt21.aufgabe1;
+
+public class Farmer {
+    private String vorname;
+    private String farmname;
+    private double vermoegen;
+    private Axt axt;
+    private Giesskanne giesskanne;
+    private int energie;
+
+
+
+}
